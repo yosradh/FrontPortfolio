04c3b5bbe912a271d014a338630fe2cf0d0ae271
yosradh <yosra.dhibi.93@gmail.com>
2024-02-07 19:00:25 +0100

    finn

--- src/Redux/QuerryAPI.jsx
+++ src/Redux/QuerryAPI.jsx
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 // Define a service using a base URL and expected endpoints
 export const querryApi = createApi({
   reducerPath: 'querryApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:1337/api/' }),
+  baseQuery: fetchBaseQuery({ baseUrl:  `${import.meta.env.VITE_BASE_URL}/api/` }),
   endpoints: (builder) => ({
     getQuerryByName: builder.query({
       query: (name) => `${name}`,
